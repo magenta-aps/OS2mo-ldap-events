62ce6e1a9bd3f8691955a9eddc6b45ca2e781f85
Lars Peter Thomsen <larsp@magenta-aps.dk>
2022-11-15 10:03:08 +0100

    [#53126] ldap listener

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,6 +1,2 @@
-from mo_ldap_events.ldap_agent import settings
-
-
 def test_initial() -> None:
-    print(settings)
     assert None is None
---
+++ tests/test_main.py
@@ -0,0 +1,146 @@
+import os
+import time
+from collections.abc import Iterator
+from datetime import datetime
+from typing import Callable
+from typing import Dict
+from unittest.mock import MagicMock
+from unittest.mock import patch
+
+import pytest
+import pytz
+from fastapi import FastAPI
+from fastramqpi.main import FastRAMQPI
+from ldap3 import ASYNC_STREAM
+from ldap3 import Connection
+from ldap3 import MOCK_ASYNC
+from ldap3 import MOCK_SYNC
+from ldap3 import Server
+
+from mo_ldap_events.ldap import datetime_to_ldap_timestamp
+from mo_ldap_events.main import create_app
+from mo_ldap_events.main import create_fastramqpi
+from mo_ldap_events.main import listener
+
+
+@pytest.fixture
+def ad_sync_connection() -> Iterator[Callable]:
+    connections: Dict[str, Connection] = {}
+
+    def method(settings, client_strategy):
+        if client_strategy == ASYNC_STREAM:
+            client_strategy = MOCK_ASYNC
+        else:
+            client_strategy = MOCK_SYNC
+        if client_strategy in connections:
+            return connections[client_strategy]
+        server = Server("fake_server")
+        connections[client_strategy] = Connection(
+            server,
+            user="cn=user,ou=test,o=lab",
+            password="my_password",
+            client_strategy=client_strategy,
+        )
+        connections[client_strategy].bind()
+        return connections[client_strategy]
+
+    yield method
+
+
+@pytest.fixture
+def fastramqpi(
+    disable_metrics: None, load_settings_overrides: dict[str, str]
+) -> Iterator[FastRAMQPI]:
+    """Fixture to construct a FastRAMQPI system.
+
+    Yields:
+        FastRAMQPI system.
+    """
+    with patch("mo_ldap_events.main.configure_ad_connection", new_callable=MagicMock):
+        yield create_fastramqpi()
+
+
+@pytest.fixture
+def app(fastramqpi: FastRAMQPI) -> Iterator[FastAPI]:
+    """Fixture to construct a FastAPI application.
+
+    Yields:
+        FastAPI application.
+    """
+    yield create_app()
+
+
+@pytest.fixture
+def settings_overrides() -> Iterator[Dict[str, str]]:
+    """Fixture to construct dictionary of minimal overrides for valid settings.
+
+    Yields:
+        Minimal set of overrides.
+    """
+    overrides = {
+        "AD_CONTROLLERS": '[{"host": "111.111.111.111"}]',
+        "CLIENT_ID": "foo",
+        "CLIENT_SECRET": "bar",
+        "AD_DOMAIN": "AD",
+        "AD_USER": "foo",
+        "AD_PASSWORD": "foo",
+        "AD_SEARCH_BASE": "DC=ad,DC=addev",
+    }
+    yield overrides
+
+
+@pytest.fixture
+def load_settings_overrides(
+    settings_overrides: Dict[str, str], monkeypatch: pytest.MonkeyPatch
+) -> Iterator[Dict[str, str]]:
+    """Fixture to set happy-path settings overrides as environmental variables.
+
+    Note:
+        Only loads environmental variables, if variables are not already set.
+
+    Args:
+        settings_overrides: The list of settings to load in.
+        monkeypatch: Pytest MonkeyPatch instance to set environmental variables.
+
+    Yields:
+        Minimal set of overrides.
+    """
+    for key, value in settings_overrides.items():
+        if os.environ.get(key) is not None:
+            continue
+        monkeypatch.setenv(key, value)
+    yield settings_overrides
+
+
+def test_poller(
+    load_settings_overrides: Dict[str, str], ad_sync_connection, mocker
+) -> None:
+    patched_listener = mocker.patch("mo_ldap_events.main.listener")
+    guid = "{e38bf5d7-342a-4fce-a38f-ca197625c98e}"
+    with patch("mo_ldap_events.main.configure_ad_connection", ad_sync_connection):
+        app = create_app()
+        app.state.context["user_context"]["ad_sync_connection"].strategy.add_entry(
+            "dc=ad,cn=tester2,ou=test,dc=ad",
+            {
+                "objectGUID": guid,
+                "cn": "tester",
+                "email": "test@example.com",
+                "modifyTimestamp": datetime_to_ldap_timestamp(
+                    datetime.now(tz=pytz.utc)
+                ),
+            },
+        )
+        time.sleep(6)  # Poller retries every 5 seconds
+        patched_listener.assert_called()
+        found_guid_lists = [
+            call.args[0].get("attributes", {}).get("objectGUID", None)
+            for call in patched_listener.call_args_list
+        ]
+        found_guids = [x for lst in found_guid_lists for x in lst]
+        assert guid in found_guids
+
+
+def test_listener():
+    # Dummy test for stub listener
+    listener({})
+    listener({"attributes": {"objectGUID": "{e38bf5d7-342a-4fce-a38f-ca197625c98e}"}})
